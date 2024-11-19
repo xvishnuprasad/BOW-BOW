91ec653f2d50d4c39c3945df8ac70f7837700cef
“xvishnuprasad” <xvishnuprasad@gmail.com>
2024-11-19 22:39:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/signuploginrealtime/ProfileActivity.java
@@ -0,0 +1,12 @@
+package com.example.signuploginrealtime;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+class profileactivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile); // Set the content to activity_edit_profile.xml
+    }
+}
